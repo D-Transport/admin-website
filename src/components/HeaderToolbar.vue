82870bc1943a6596967a034104954643fcb3c3d9
Coac <levictor3000@hotmail.fr>
2017-04-06 19:35:44 +0200

    Header toolbar dynamic balance and coinbase value

--- src/components/HeaderToolbar.vue
+++ src/components/HeaderToolbar.vue
@@ -5,22 +5,23 @@
     </md-button>
 
     <h2 class="md-title" style="flex: 1">D-Transport</h2>
-    <h3 class="md-title" style="flex: 1">Balance : 21231 ETH</h3>
-    <h3 class="md-title" style="flex: 1">Connected as 0x1651651651654654151</h3>
+    <h3 class="md-title" style="flex: 1">Balance : {{balance}} ETH</h3>
+    <h3 class="md-title" style="flex: 1">Connected as {{address}}</h3>
 
   </md-toolbar>
 </template>
 
 <script>
-import DTransportInstance from '../config/DTransport';
 import web3 from '../config/web3';
 
-const coinbase = web3.eth.coinbase;
-const balance = web3.eth.getBalance(coinbase);
-console.log(balance);
-console.log(DTransportInstance);
-
 export default {
-
+  data: () => {
+    const coinbase = web3.eth.coinbase;
+    const balance = web3.eth.getBalance(coinbase);
+    return {
+      balance,
+      address: coinbase,
+    };
+  },
 };
 </script>
