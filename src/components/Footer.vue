eeb7f2fdb16c77d55a38498c82f4d200b690db8a
Coac <levictor3000@hotmail.fr>
2017-04-06 20:51:51 +0200

    Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer class="blue">
+    <div class="text-xs-left">© 2017 <a href="https://github.com/D-Transport" style="color: white">D-Transport</a></div>
+    <v-btn flat light class="teal--text">
+     <span>Favorites</span>
+     <v-icon>favorite</v-icon>
+   </v-btn>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+};
+</script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@
     <div id="content">
       <blockquote> Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.</blockquote>
     </div>
+    <my-footer></my-footer>
   </div>
 </template>
 
@@ -17,12 +18,14 @@
 <script>
 import DTransportInstance from '../config/DTransport';
 import HeaderToolbar from './HeaderToolbar';
+import Footer from './Footer';
 
 console.log(DTransportInstance);
 
 export default {
   components: {
     'header-toolbar': HeaderToolbar,
+    'my-footer': Footer,
   },
 };
 </script>
