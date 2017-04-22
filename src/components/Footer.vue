209508bab90a7e3452e1c5b07e237dd67dbbc9fe
Coac <levictor3000@hotmail.fr>
2017-04-22 13:36:16 +0200

    Remove footer button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,6 @@
 <template>
   <v-footer class="blue">
     <div class="text-xs-left">© 2017 <a href="https://github.com/D-Transport" style="color: white">D-Transport</a></div>
-    <v-btn flat light class="teal--text">
-     <span>Favorites</span>
-     <v-icon>favorite</v-icon>
-   </v-btn>
   </v-footer>
 </template>
 
