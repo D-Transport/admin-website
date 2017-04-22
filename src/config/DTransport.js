266591e4e7cc76b019c127f9ffaff8f4bf7c798e
Coac <levictor3000@hotmail.fr>
2017-04-22 13:25:03 +0200

    Update submodule

--- src/config/DTransport.js
+++ src/config/DTransport.js
@@ -3,7 +3,7 @@ import web3 from './web3';
 const DTransport = require('../../smart-contract/build/contracts/DTransport.json');
 
 // const address = DTransport.networks['1491045995487'].address;
-const address = '0x2aea34aed003907abb067ae5c1d397ec5d8921c1';
+const address = '0x4232e8d089cb21ae5fd9e242703d382c53c89eab';
 const DTransportContract = web3.eth.contract(DTransport.abi);
 const DTransportInstance = DTransportContract.at(address);
 
